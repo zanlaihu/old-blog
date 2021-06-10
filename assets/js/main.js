fb46b6b6f4be90b2d193a9b724de68148deff84e
胡赞來 <361206768@qq.com>
2021-06-10 13:31:50 +0800

    添加多个隐藏方法

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 $(function () {
+    function canToggle(){
+        if ($(".mobile-nav").css("display") == "block") {
+            $(".mobile-nav").animate({ height: "toggle" }, "fast");
+        }
+    }
+
     $(".mobile-nav-control").click(function () {
         $(".mobile-nav").animate({ height: "toggle" }, "fast");
     });
+
+    $(".home-ins").click(function () {canToggle()});
+
+    $(".mobile-nav-bottom").click(function () {canToggle()});
 });
