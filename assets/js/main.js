d06fbf8fc0d07a37dbcb34f9796ee8f58622fe08
胡赞來 <361206768@qq.com>
2021-06-10 13:36:28 +0800

    试试看

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,8 @@ $(function () {
         $(".mobile-nav").animate({ height: "toggle" }, "fast");
     });
 
-    $(".home-ins").click(function () {canToggle()});
+    // $(".home-ins").click(function () {canToggle()});
+    $(".home-ins").mouseenter(function () {canToggle()});
 
     $(".mobile-nav-bottom").click(function () {canToggle()});
 });
