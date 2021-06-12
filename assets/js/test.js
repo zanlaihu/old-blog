7f1871bb6d63b32ef4ded8033a6f03c4e5dd7441
胡赞來 <361206768@qq.com>
2021-06-12 16:00:51 +0800

    重制navigation

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,20 +1,10 @@
 $(function () {
-    // function canToggle(){
-    //     if ($(".mobile-nav").css("display") == "block") {
-    //         $(".mobile-nav").animate({ height: "toggle" });
-    //     }
-    // }
-
-    $(".test-navigation").click(function () {
-        if($(".test-dropdown").css("height") == '600px'){
-            $(".test-dropdown").animate({ height: "60px" });
-        } else {
-            $(".test-dropdown").animate({ height: "600px" });
-        }
-    });
-
-    // $(".home-ins").click(function () {canToggle()});
-    $(".home-ins").mouseenter(function () {canToggle()});
-
-    $(".mobile-nav-bottom").click(function () {canToggle()});
+  // navigation
+  $(".navigation").click(function () {
+    if ($(".dropdown").css("height") == "300px") {
+      $(".dropdown").animate({ height: "50px" });
+    } else {
+      $(".dropdown").animate({ height: "300px" });
+    }
+  });
 });
