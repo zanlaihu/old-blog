f1c33f93ab37a30dcb7b21310d3663b63b99a248
胡赞來 <361206768@qq.com>
2021-06-13 04:36:15 +0800

    增加了会旋转的按钮

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 $(function () {
-  // navigation
-  $(".navigation").click(function () {
+  //点击菜单按钮事件
+  let i = 1;
+  $(".inner-header-icon").click(function () {
+    if (i > 0) {
+      i = i - 1;
+      $(this).addClass("inner-header-icon-click");
+      $(this).removeClass("inner-header-icon-out");
+    } else {
+      i = i + 1;
+      $(this).removeClass("inner-header-icon-click");
+      $(this).addClass("inner-header-icon-out");
+    }
+    // $(".inner-nav").slideToggle(250);
     if ($(".dropdown").css("height") == "300px") {
       $(".dropdown").animate({ height: "50px" });
     } else {
       $(".dropdown").animate({ height: "300px" });
     }
+  });
+  //
+  $(".dropdown-menu a").each(function (index) {
+    $(this).css({ "animation-delay": index / 10 + "s" });
   });
 });
