6a4c8c18959db9a945130bd75f5aa3cc5cfd95f7
胡赞來 <361206768@qq.com>
2021-06-14 14:29:10 +0800

    [modify]

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,7 @@
 $(function () {
   const js =
     "<a><div class='note-title'>JavaScript</div></a><a><div>1 什么是JavaScript</div></a><a><div>2 script标签</div></a><a><div>3 语法</div></a><a><div>4 var,let,const</div></a><a><div>5 语法</div></a><a><div>6 var,let,const</div></a><a><div>7 数据类型</div></a><a><div>8 对象</div></a><a><div>9 操作符</div></a><a><div>10 语句</div></a>";
+  const react = "<a><div class='note-title'>React</div></a><a><div>1 什么是React</div></a><a><div>2 什么是JavaScript</div></a>";
 
   $(".sidebar").append(js);
 });
