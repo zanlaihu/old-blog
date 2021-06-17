882983790a2afbae139d6ca186ea1ffae71b98b4
胡赞來 <361206768@qq.com>
2021-06-17 23:52:53 +0800

    modify

--- assets/js/js-common.js
+++ assets/js/js-common.js
@@ -1,6 +1,5 @@
 $(function () {
-  const js =
-    "<a href='js.html'><div class='note-title'>JavaScript</div></a>";
+  const js = "<a href='js.html'><div class='note-title'>JavaScript</div></a>";
   const js1 = "<a href='js-1.html'><div>1 什么是JavaScript</div></a>";
   const js2 = "<a href='js-2.html'><div>2 script标签</div></a>";
   const js3 = "<a href='js-3.html'><div>3 语法</div></a>";
@@ -9,7 +8,8 @@ $(function () {
   const js6 = "<a href='js-6.html'><div>6 数据类型之对象</div></a>";
   const js7 = "<a href='js-7.html'><div>7 操作符</div></a>";
   const js8 = "<a href='js-8.html'><div>8 语句</div></a>";
-  $(".sidebar").append(js, js1, js2, js3, js4, js5, js6, js7, js8);
+  const jsqa = "<a href='js-qa.html'><div>JavaScript问题汇总</div></a>";
+  $(".sidebar").append(js, js1, js2, js3, js4, js5, js6, js7, js8, jsqa);
 
   $(".dropdown-menu a:nth-of-type(1)").attr("href", "../../index.html");
   $(".dropdown-menu a:nth-of-type(2)").attr("href", "../../study-notes.html");
@@ -18,7 +18,10 @@ $(function () {
   $(".dropdown-menu a:nth-of-type(5)").attr("href", "../../about.html");
 
   $(".navigation-choice a:nth-of-type(1)").attr("href", "../../index.html");
-  $(".navigation-choice a:nth-of-type(2)").attr("href", "../../study-notes.html");
+  $(".navigation-choice a:nth-of-type(2)").attr(
+    "href",
+    "../../study-notes.html"
+  );
   $(".navigation-choice a:nth-of-type(3)").attr("href", "../../skills.html");
   $(".navigation-choice a:nth-of-type(4)").attr("href", "../../projects.html");
   $(".navigation-choice a:nth-of-type(5)").attr("href", "../../about.html");
