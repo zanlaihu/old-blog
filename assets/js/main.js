e1d523e731d2eafe983a743d564882e93a79de6d
胡赞來 <361206768@qq.com>
2021-07-12 14:44:30 +0800

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,22 +7,22 @@ $(function () {
     // 交叉横线变化
     if (i > 0) {
       i = i - 1;
+      // =变叉
       $(this).addClass("inner-header-icon-click");
       $(this).removeClass("inner-header-icon-out");
+      // 向下拉伸
       $(".dropdown").animate({ height: "350px" });
     } else {
       i = i + 1;
+      // 叉变=
       $(this).removeClass("inner-header-icon-click");
       $(this).addClass("inner-header-icon-out");
+      // 向上收回
       $(".dropdown").animate({ height: "40px" });
     }
 
     // 下拉弹出选项
     $(".dropdown-menu").slideToggle();
     $(".dropdown-line").fadeToggle("fast");
-  });
-
-  $(".inner-header-icon").hover(function () {
-    $(".inner-header-icon span").animate({ backgroud: "white" });
   });
 });
